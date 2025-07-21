652cfe3eae3cf35c0abe5728183f5b14c34501ea
Saulo <saulo@inngage.com.br>
2025-07-21 10:18:25 -0300

    chore: update SDWebImageSwiftUI to 3.1.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0")
+        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "3.1.3")
     ],
     targets: [
         .target(
